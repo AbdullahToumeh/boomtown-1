33c865b089679505c4fabf27a3344c32e0aea421
Dakota M <dakota.paige@hotmail.com>
2018-04-22 18:41:09 -0700

    Added .catch error handling to profile module

--- client/src/redux/modules/items.js
+++ client/src/redux/modules/items.js
@@ -4,90 +4,86 @@ const GET_IS_LOADING = 'GET_IS_LOADING';
 const GET_ERROR = 'GET_ERROR';
 const GET_ITEM_FILTERS = 'GET_ITEM_FILTERS';
 
-
 // Creating action creators
-export const get_fetch_items = (items) => ({
-  type: GET_FETCH_ITEMS,
-  payload: items
+export const get_fetch_items = items => ({
+    type: GET_FETCH_ITEMS,
+    payload: items
 });
 
 export const get_isLoading = () => ({
-  type: GET_IS_LOADING
+    type: GET_IS_LOADING
 });
 
-export const get_error = (error) => ({
-  type: GET_ERROR,
-  payload: error
+export const get_error = error => ({
+    type: GET_ERROR,
+    payload: error
 });
 
-export const get_itemFilters = (itemFilters) => ({
-  type: GET_ITEM_FILTERS,
-  payload: itemFilters
+export const get_itemFilters = itemFilters => ({
+    type: GET_ITEM_FILTERS,
+    payload: itemFilters
 });
-
 
 // creating initial state
 const initialState = {
-  items: [],
-  isLoading: false,
-  itemFilters: [],
-  error: ''
+    items: [],
+    isLoading: false,
+    itemFilters: [],
+    error: ''
 };
-
 
 // CREATING OUR THUNK FETCH ACTION
 export const fetchItemsFromUrl = urls => dispatch => {
-  // const urls = ['http://localhost:3000/items', 'http://localhost:3000/users'];
+    // const urls = ['http://localhost:3000/items', 'http://localhost:3000/users'];
 
-  const combineItemsAndUsers = (itemsAndUsers) => {
-    itemsAndUsers[0].map(item => {
-      itemsAndUsers[1].map(user => {
-        if (user.id === item.itemowner) {
-          item.itemowner = user;
-        }
-      });
-    });
-    return itemsAndUsers[0];
-  };
-  dispatch(get_isLoading());
-  Promise.all(urls.map(url =>
-    fetch(url).then(resp => resp.json())
-  ))
-  .then(responses => dispatch(get_fetch_items(combineItemsAndUsers(responses))))
-  .catch(error => dispatch(get_error(error)));
+    const combineItemsAndUsers = itemsAndUsers => {
+        itemsAndUsers[0].map(item => {
+            itemsAndUsers[1].map(user => {
+                if (user.id === item.itemowner) {
+                    item.itemowner = user;
+                }
+            });
+        });
+        return itemsAndUsers[0];
+    };
+    dispatch(get_isLoading());
+    Promise.all(urls.map(url => fetch(url).then(resp => resp.json())))
+        .then(responses =>
+            dispatch(get_fetch_items(combineItemsAndUsers(responses)))
+        )
+        .catch(error => dispatch(get_error(error)));
 };
 
 // creating reducer
 
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case GET_FETCH_ITEMS: {
-      const items = action.payload;
-      return { ...state, items, isLoading: false, error: '' };
-    }
-    case GET_IS_LOADING: {
-      return { ...state, isLoading: true, error: '' };
-    }
-    case GET_ITEM_FILTERS: {
-      const itemFilters = [...state.itemFilters];
-      if (!itemFilters.includes(action.payload)) {
-        itemFilters.push(action.payload);
-      }
-      else {
-        const index = itemFilters.indexOf(action.payload);
-        if (index > -1) {
-          itemFilters.splice(index, 1);
+    switch (action.type) {
+        case GET_FETCH_ITEMS: {
+            const items = action.payload;
+            return { ...state, items, isLoading: false, error: '' };
         }
-      }
-      return { ...state, itemFilters };
+        case GET_IS_LOADING: {
+            return { ...state, isLoading: true, error: '' };
+        }
+        case GET_ITEM_FILTERS: {
+            const itemFilters = [...state.itemFilters];
+            if (!itemFilters.includes(action.payload)) {
+                itemFilters.push(action.payload);
+            } else {
+                const index = itemFilters.indexOf(action.payload);
+                if (index > -1) {
+                    itemFilters.splice(index, 1);
+                }
+            }
+            return { ...state, itemFilters };
+        }
+        case GET_ERROR: {
+            return { ...state, isLoading: false, error: action.payload };
+        }
+        default: {
+            return {
+                ...state
+            };
+        }
     }
-    case GET_ERROR: {
-      return { ...state, isLoading: false, error: action.payload };
-    }
-    default: {
-      return {
-        ...state
-      };
-    }
-  }
 };
--- client/src/redux/modules/profile.js
+++ client/src/redux/modules/profile.js
@@ -1,69 +1,84 @@
 // Defining actions
-const GET_PROFILE_ITEMS = "GET_PROFILE_ITEMS";
-const GET_IS_LOADING = "GET_IS_LOADING";
+const GET_PROFILE_ITEMS = 'GET_PROFILE_ITEMS';
+const GET_IS_LOADING = 'GET_IS_LOADING';
+const GET_ERROR = 'GET_ERROR';
 
 // Creating action creators
 
 export const get_profile_items = items => ({
-  type: GET_PROFILE_ITEMS,
-  payload: items
+    type: GET_PROFILE_ITEMS,
+    payload: items
 });
 
 export const get_is_loading = () => ({
-  type: GET_IS_LOADING
+    type: GET_IS_LOADING
+});
+
+export const get_error = error => ({
+    type: GET_ERROR,
+    payload: error
 });
 
 // creating initial state
 const initialState = {
-  profileItems: [],
-  isLoading: false
+    profileItems: [],
+    isLoading: false,
+    error: ''
 };
 
 // CREATING OUR THUNK FETCH ACTION
 
 export const fetchProfileItemsFromUrl = profileId => dispatch => {
-  const urls = ["http://localhost:3000/items", "http://localhost:3000/users"];
+    const urls = ['http://localhost:3000/items', 'http://localhost:3000/users'];
 
-  const combineItemsAndUsers = itemsAndUsers => {
-    itemsAndUsers[0].map(item => {
-      itemsAndUsers[1].map(user => {
-        if (user.id === item.itemowner) {
-          item.itemowner = user;
-        }
-      });
-    });
-    return itemsAndUsers[0];
-  };
-  dispatch(get_is_loading());
-  const matchProfileItems = (items, profileId) => {
-    return items.filter(item => item.itemowner.id === profileId);
-  };
+    const combineItemsAndUsers = itemsAndUsers => {
+        itemsAndUsers[0].map(item => {
+            itemsAndUsers[1].map(user => {
+                if (user.id === item.itemowner) {
+                    item.itemowner = user;
+                }
+            });
+        });
+        return itemsAndUsers[0];
+    };
+    dispatch(get_is_loading());
 
-  Promise.all(urls.map(url => fetch(url).then(resp => resp.json()))).then(
-    responses =>
-      dispatch(
-        get_profile_items(
-          matchProfileItems(combineItemsAndUsers(responses), profileId)
+    const matchProfileItems = (items, profileId) => {
+        return items.filter(item => item.itemowner.id === profileId);
+    };
+
+    Promise.all(urls.map(url => fetch(url).then(resp => resp.json())))
+        .then(responses =>
+            dispatch(
+                get_profile_items(
+                    matchProfileItems(
+                        combineItemsAndUsers(responses),
+                        profileId
+                    )
+                )
+            )
         )
-      )
-  );
+        .catch(error => dispatch(get_error(error)));
 };
 
 // creating reducer
 
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case GET_PROFILE_ITEMS: {
-      const profileItems = [...action.payload];
-      return { ...state, profileItems, isLoading: false };
+    switch (action.type) {
+        case GET_PROFILE_ITEMS: {
+            const profileItems = [...action.payload];
+            return { ...state, profileItems, isLoading: false };
+        }
+        case GET_IS_LOADING: {
+            return { ...state, isLoading: true };
+        }
+        case GET_ERROR: {
+            return { ...state, isLoading: false, error: action.payload };
+        }
+        default: {
+            return {
+                ...state
+            };
+        }
     }
-    case GET_IS_LOADING: {
-      return { ...state, isLoading: true };
-    }
-    default: {
-      return {
-        ...state
-      };
-    }
-  }
 };
