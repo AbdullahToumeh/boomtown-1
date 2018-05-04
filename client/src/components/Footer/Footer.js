ce97db78022c106431a0b75124c8664cc172a019
Dakota M <dakota.paige@hotmail.com>
2018-05-03 20:24:07 -0700

    Added a + button to footer to route to share page, might have to move it out of footer later

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import { Link } from 'react-router-dom';
+
+import './style.css';
 
 const Footer = () => (
     <div>
         <p>&copy; 2018 Boomtown Corp. All Rights Reserved</p>
+        <Link to="/Share" className="share-button">
+            <FloatingActionButton secondary>
+                <ContentAdd />
+            </FloatingActionButton>
+        </Link>
     </div>
 );
 
