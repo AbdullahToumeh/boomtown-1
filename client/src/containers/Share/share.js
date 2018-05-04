770948839305c4a694458f9d45c2add45ee9356c
Dakota M <dakota.paige@hotmail.com>
2018-05-03 19:32:23 -0700

    Added MaterialUI for form stepper on share page

--- client/src/containers/Share/share.js
+++ client/src/containers/Share/share.js
@@ -4,8 +4,52 @@ import './style.css';
 
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import { Step, Stepper, StepButton, StepContent } from 'material-ui/Stepper';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 
 export default class Share extends Component {
+    state = {
+        stepIndex: 0
+    };
+
+    handleNext = () => {
+        const { stepIndex } = this.state;
+        if (stepIndex < 3) {
+            this.setState({ stepIndex: stepIndex + 1 });
+        }
+    };
+
+    handlePrev = () => {
+        const { stepIndex } = this.state;
+        if (stepIndex > 0) {
+            this.setState({ stepIndex: stepIndex - 1 });
+        }
+    };
+
+    renderStepActions(step) {
+        return (
+            <div style={{ margin: '12px 0' }}>
+                <RaisedButton
+                    label="Next"
+                    disableTouchRipple={true}
+                    disableFocusRipple={true}
+                    primary={true}
+                    onClick={this.handleNext}
+                    style={{ marginRight: 12 }}
+                />
+                {step > 0 && (
+                    <FlatButton
+                        label="Back"
+                        disableTouchRipple={true}
+                        disableFocusRipple={true}
+                        onClick={this.handlePrev}
+                    />
+                )}
+            </div>
+        );
+    }
+
     onSubmit(values) {
         console.log('Form was submitted', values);
     }
@@ -19,6 +63,7 @@ export default class Share extends Component {
     }
 
     render() {
+        const { stepIndex } = this.state;
         return (
             <div className="share-main">
                 <div className="preview-item">
@@ -40,116 +85,186 @@ export default class Share extends Component {
                                 handleSubmit(values);
                             }}
                         >
-                            <label>Upload an Image</label>
-                            <Field
-                                name="image"
-                                validate={this.required.bind(this)}
+                            <Stepper
+                                activeStep={stepIndex}
+                                linear={false}
+                                orientation="vertical"
                             >
-                                {({ input, meta }) => {
-                                    meta.touched &&
-                                        meta.error && <span>{meta.error}</span>;
-                                    return (
-                                        <input
-                                            type="file"
-                                            accept="image/*"
-                                            {...input}
-                                            required
-                                        />
-                                    );
-                                }}
-                            </Field>
-
-                            <label>Title</label>
-                            <Field
-                                name="title"
-                                validate={this.required.bind(this)}
-                            >
-                                {({ input, meta }) => {
-                                    meta.touched &&
-                                        meta.error && <span>{meta.error}</span>;
-                                    return (
-                                        <input
-                                            type="text"
-                                            required
-                                            {...input}
-                                        />
-                                    );
-                                }}
-                            </Field>
-
-                            <label>Description</label>
-                            <Field
-                                name="description"
-                                validate={this.required.bind(this)}
-                            >
-                                {({ input, meta }) => {
-                                    meta.touched &&
-                                        meta.error && <span>{meta.error}</span>;
-                                    return (
-                                        <input
-                                            type="textarea"
-                                            required
-                                            {...input}
-                                        />
-                                    );
-                                }}
-                            </Field>
-
-                            <label>Select some Tags</label>
-                            <Field
-                                name="tags"
-                                validate={this.required.bind(this)}
-                            >
-                                {({ input, meta }) => {
-                                    meta.touched &&
-                                        meta.error && <span>{meta.error}</span>;
-                                    return (
-                                        <SelectField
-                                            multiple
-                                            value="tags"
-                                            hintText="Select Category Tags"
+                                <Step>
+                                    <StepButton
+                                        onClick={() =>
+                                            this.setState({ stepIndex: 0 })
+                                        }
+                                    >
+                                        Add an Image
+                                    </StepButton>
+                                    <StepContent>
+                                        <label>Upload an Image</label>
+                                        <Field
+                                            name="image"
+                                            validate={this.required.bind(this)}
                                         >
-                                            <MenuItem
-                                                key="1"
-                                                insetChildren
-                                                checked
-                                                value="1"
-                                                primaryText="Household Items"
-                                            />
-                                            <MenuItem
-                                                key="2"
-                                                insetChildren
-                                                checked
-                                                value="1"
-                                                primaryText="Recreational Equipment"
-                                            />
-                                            <MenuItem
-                                                key="3"
-                                                insetChildren
-                                                checked
-                                                value="1"
-                                                primaryText="Tools"
-                                            />
-                                            <MenuItem
-                                                key="4"
-                                                insetChildren
-                                                checked
-                                                value="1"
-                                                primaryText="Phyical Media"
-                                            />
-                                            <MenuItem
-                                                key="5"
-                                                insetChildren
-                                                checked
-                                                value="1"
-                                                primaryText="Sports"
-                                            />
-                                        </SelectField>
-                                    );
-                                }}
-                            </Field>
+                                            {({ input, meta }) => {
+                                                meta.touched &&
+                                                    meta.error && (
+                                                        <span>
+                                                            {meta.error}
+                                                        </span>
+                                                    );
+                                                return (
+                                                    <input
+                                                        type="file"
+                                                        accept="image/*"
+                                                        {...input}
+                                                        required
+                                                    />
+                                                );
+                                            }}
+                                        </Field>
+                                        {this.renderStepActions(0)}
+                                    </StepContent>
+                                </Step>
 
-                            <button type="submit">Submit</button>
+                                <Step>
+                                    <StepButton
+                                        onClick={() =>
+                                            this.setState({ stepIndex: 1 })
+                                        }
+                                    >
+                                        Add a Title & Description
+                                    </StepButton>
+                                    <StepContent>
+                                        <label>Title</label>
+                                        <Field
+                                            name="title"
+                                            validate={this.required.bind(this)}
+                                        >
+                                            {({ input, meta }) => {
+                                                meta.touched &&
+                                                    meta.error && (
+                                                        <span>
+                                                            {meta.error}
+                                                        </span>
+                                                    );
+                                                return (
+                                                    <input
+                                                        type="text"
+                                                        required
+                                                        {...input}
+                                                    />
+                                                );
+                                            }}
+                                        </Field>
+
+                                        <label>Description</label>
+                                        <Field
+                                            name="description"
+                                            validate={this.required.bind(this)}
+                                        >
+                                            {({ input, meta }) => {
+                                                meta.touched &&
+                                                    meta.error && (
+                                                        <span>
+                                                            {meta.error}
+                                                        </span>
+                                                    );
+                                                return (
+                                                    <input
+                                                        type="textarea"
+                                                        required
+                                                        {...input}
+                                                    />
+                                                );
+                                            }}
+                                        </Field>
+                                        {this.renderStepActions(1)}
+                                    </StepContent>
+                                </Step>
+
+                                <Step>
+                                    <StepButton
+                                        onClick={() =>
+                                            this.setState({ stepIndex: 2 })
+                                        }
+                                    >
+                                        Categorize your Item
+                                    </StepButton>
+                                    <StepContent>
+                                        <label>Select some Tags</label>
+                                        <Field
+                                            name="tags"
+                                            validate={this.required.bind(this)}
+                                        >
+                                            {({ input, meta }) => {
+                                                meta.touched &&
+                                                    meta.error && (
+                                                        <span>
+                                                            {meta.error}
+                                                        </span>
+                                                    );
+                                                return (
+                                                    <SelectField
+                                                        multiple
+                                                        value="tags"
+                                                        hintText="Select Category Tags"
+                                                    >
+                                                        <MenuItem
+                                                            key="1"
+                                                            insetChildren
+                                                            checked
+                                                            value="1"
+                                                            primaryText="Household Items"
+                                                        />
+                                                        <MenuItem
+                                                            key="2"
+                                                            insetChildren
+                                                            checked
+                                                            value="1"
+                                                            primaryText="Recreational Equipment"
+                                                        />
+                                                        <MenuItem
+                                                            key="3"
+                                                            insetChildren
+                                                            checked
+                                                            value="1"
+                                                            primaryText="Tools"
+                                                        />
+                                                        <MenuItem
+                                                            key="4"
+                                                            insetChildren
+                                                            checked
+                                                            value="1"
+                                                            primaryText="Phyical Media"
+                                                        />
+                                                        <MenuItem
+                                                            key="5"
+                                                            insetChildren
+                                                            checked
+                                                            value="1"
+                                                            primaryText="Sports"
+                                                        />
+                                                    </SelectField>
+                                                );
+                                            }}
+                                        </Field>
+                                        {this.renderStepActions(2)}
+                                    </StepContent>
+                                </Step>
+
+                                <Step>
+                                    <StepButton
+                                        onClick={() =>
+                                            this.setState({ stepIndex: 3 })
+                                        }
+                                    >
+                                        Confirm Things!
+                                    </StepButton>
+                                    <StepContent>
+                                        <button type="submit">Submit</button>
+                                        {this.renderStepActions(3)}
+                                    </StepContent>
+                                </Step>
+                            </Stepper>
                         </form>
                     )}
                 />
